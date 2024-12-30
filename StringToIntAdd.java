412cccfb93b74d1bd58b65dd6b5e9f6066d12523
AnushaH <165499819+AnushaH13@users.noreply.github.com>
2024-12-31 05:20:26 +0530

    This is a program for convert string to integer and addition of integer

---
+++ StringToIntAdd.java
@@ -0,0 +1,9 @@
+public class StringToIntAdd {
+
+	public static void main(String[] args) {
+		String s1="110";
+		String s2="224";
+		int a=Integer.parseInt(s1)+Integer.parseInt(s2);
+		System.out.println("Addition of s1 and s2 is: "+a);
+	}
+}
