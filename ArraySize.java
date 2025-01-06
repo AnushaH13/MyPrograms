70d5ce45ea716063dd83b661abc66b86e00d17d2
AnushaH <165499819+AnushaH13@users.noreply.github.com>
2025-01-07 04:46:12 +0530

    This program prints the size of an array without using inbuilt method

---
+++ ArraySize.java
@@ -0,0 +1,15 @@
+
+public class ArraySize {
+
+	public static void main(String[] args) {
+		int[]arr= {10,20,30,40,50};
+		int count=0;
+		for(int i:arr)
+		{
+			count++;
+		}
+		System.out.println("The array length is: "+count);
+
+	}
+
+}
